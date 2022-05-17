a89baaf9146fa9bc79235863d4abeb82f3a9d875
Kowal <bartlomiej319@wp.pl>
2022-05-17 15:56:18 +0200

    State Controller and template HTML done with dropdown list of countries already added in country record

--- src/main/java/com/example/fleetmanagement/parameters/controllers/StateController.java
+++ src/main/java/com/example/fleetmanagement/parameters/controllers/StateController.java
@@ -1,12 +1,69 @@
 package com.example.fleetmanagement.parameters.controllers;
 
+import com.example.fleetmanagement.parameters.models.Country;
+import com.example.fleetmanagement.parameters.models.State;
+import com.example.fleetmanagement.parameters.services.CountryService;
+import com.example.fleetmanagement.parameters.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class StateController {
-    @GetMapping("/state")
-    public String getStates(){
-        return "state";
+    @Autowired
+    private StateService stateService;
+    @Autowired private CountryService countryService;
+
+    public Model addModelAttribute(Model model){
+        model.addAttribute("states", stateService.findAll());
+        model.addAttribute("countries", countryService.findAll());
+        return model;
+    }
+
+    @GetMapping("states")
+    public String getAll(Model model, String keyword) {
+        List<State> states = stateService.findAll();
+        model.addAttribute("states", states);
+        return "parameters/states";
+    }
+
+    @GetMapping("stateAdd")
+    public String addState(Model model) {
+        addModelAttribute(model);
+        return "parameters/stateAdd";
+    }
+
+    @PostMapping("states")
+    public String save(State state){
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+    @RequestMapping(value="/states/delete/{id}", method = { RequestMethod.GET, RequestMethod.DELETE})
+    public String delete(@PathVariable Integer id){
+        stateService.delete(id);
+        return "redirect:/states";
+    }
+
+    @GetMapping("state/{op}/{id}")
+    public String editState(@PathVariable Integer id, @PathVariable String op, Model model) {
+        addModelAttribute(model);
+        model.addAttribute("state", stateService.getById(id));
+        return "parameters/state" + op;
+    }
+    @GetMapping("state/Details/{id}")
+    public String detailsState(@PathVariable Integer id, Model model) {
+        State state = stateService.getById(id);
+        model.addAttribute("state", state);
+        return "parameters/stateDetails";
+    }
+
+    @RequestMapping(value="/states/update/{id}", method = { RequestMethod.GET, RequestMethod.PUT})
+    public String update(State state){
+        stateService.save(state);
+        return "redirect:/states";
     }
 }
--- src/main/java/com/example/fleetmanagement/parameters/services/StateService.java
+++ src/main/java/com/example/fleetmanagement/parameters/services/StateService.java
@@ -1,8 +1,33 @@
 package com.example.fleetmanagement.parameters.services;
 
+import com.example.fleetmanagement.parameters.models.State;
+import com.example.fleetmanagement.parameters.repository.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StateService {
+
+    @Autowired
+    private StateRepository stateRepository;
+
+    public List<State> findAll(){
+        return stateRepository.findAll();
+    }
+    public State getById(int id){
+        return stateRepository.findById(id).orElse(null);
+    }
+
+    public void delete(int id){
+         stateRepository.deleteById(id);
+    }
+
+    public void save(State state){
+        stateRepository.save(state);
+    }
+
+
 
 }
